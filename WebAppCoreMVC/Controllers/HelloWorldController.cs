254637be1cc1ce791ddd2b8a12183a51e29e54c6
profjuanpablo <prof.juanpablo@gmail.com>
2022-11-25 15:52:09 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppCoreMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebAppCoreMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes) 
+        {
+            ViewData["Msg"] = "Olá " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
